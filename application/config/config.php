7d8300d2f1a0ce6dbad905a023d5e99cd5ab1cb7
sferawann <sferawann@github.com>
2021-05-25 02:17:45 +0700

    post

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/personal_blog';
+$config['base_url'] = 'http://dewirosmala.online/';
 
 /*
 |--------------------------------------------------------------------------
